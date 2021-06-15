664b906a91a6f62d2f4428fd38f6f59101afd485
Andrey Rasskazov <ranzansan@gmail.com>
2021-06-15 12:34:08 +0300

    Добавьте файлы проекта.

---
+++ productOfTwoVar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productOfTwoVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int oneNum = int.Parse(Console.ReadLine());
+            int twoNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(oneNum * twoNum);
+            Console.ReadKey();
+        }
+    }
+}
